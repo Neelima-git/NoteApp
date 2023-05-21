fd6dcc42ecdd5d92e72c412cbd75dd339b70a990
nbellana <nbellana@innominds.com>
2023-05-21 23:08:30 +0530

    Updated styling to tailwind css

--- src/App.vue
+++ src/App.vue
@@ -30,156 +30,26 @@ const addNote = () => {
 </script>
 
 <template>
-  <main>
-    <div v-show="showModal" class="overlay">
-      <div class="modal">
-        <textarea
-          v-model.trim="newNoteInput"
-          name="note"
-          id="note"
-          cols="30"
-          rows="10"
-        ></textarea>
-        <p v-if="errorMessage">{{ errorMessage }}</p>
-        <button @click="addNote">Add Note</button>
-        <button class="close" @click="showModal = false">Close</button>
+  <main class="h-screen w-screen">
+    <div v-show="showModal" class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-75">
+      <div class="w-96 bg-white rounded-lg p-6">
+        <textarea v-model.trim="newNoteInput" name="note" id="note" cols="30" rows="10" class="w-full h-32 p-2 text-lg"></textarea>
+        <p v-if="errorMessage" class="text-red-500">{{ errorMessage }}</p>
+        <button @click="addNote" class="w-full py-2 mt-3 text-white bg-blue-500 rounded-lg">Add Note</button>
+        <button class="w-full py-2 mt-3 text-white bg-red-500 rounded-lg" @click="showModal = false">Close</button>
       </div>
     </div>
-    <div class="container">
-      <header>
-        <h1>Notes</h1>
-        <button @click="showModal = true">+</button>
+    <div class="container mx-auto p-10">
+      <header class="flex justify-between items-center">
+        <h1 class="font-bold text-6xl mb-10">Notes</h1>
+        <button @click="showModal = true" class="w-16 h-16 bg-black rounded-full text-white text-4xl flex items-center justify-center">+</button>
       </header>
-      <div class="cards-container">
-        <div
-          v-for="note in notes"
-          :key="note.id"
-          class="card"
-          :style="{ backgroundColor: note.backgroundColor }"
-        >
-          <p class="main-text">{{ note.text }}</p>
+      <div class="flex flex-wrap -mr-4 -mb-4 p-4">
+        <div v-for="note in notes" :key="note.id" class="card mr-4 mb-4 p-2  w-56 max-h-64 overflow-y-auto" :style="{ backgroundColor: note.backgroundColor }">
+          <p class="main-text whitespace-wrap break-words">{{ note.text }}</p>
           <p class="date">{{ note.date.toLocaleDateString("en-US") }}</p>
         </div>
       </div>
     </div>
   </main>
 </template>
-
-<style scoped>
-.container {
-    max-width: 1000px;
-    padding: 10px;
-    margin: 0 auto
-  }
-
-  h1 {
-    font-weight: bold;
-    margin-bottom: 25px;
-    font-size: 75px;
-  }
-
-  .card {
-    width: 225px;
-    height: 225px;
-    background-color: rgb(237, 182, 44);
-    padding: 10px;
-    border-radius: 15px;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    margin-right: 20px;
-    margin-bottom: 20px;
-  }
-
-  .card:hover {
-    cursor: pointer;
-    opacity: 0.9;
-  }
-
-  .main-text {
-    line-height: 1.25;
-    font-size: 17.5px;
-    font-weight: bold;
-  }
-
-  .date {
-    font-size: 12.5px;
-    margin-top: auto;
-  }
-
-  header {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-  }
-
-  header button {
-    border: none;
-    padding: 10px;
-    width: 50px;
-    height: 50px;
-    cursor: pointer;
-    background-color: rgb(21, 20, 20);
-    border-radius: 1000px;
-    color: white;
-    font-size: 20px;
-  }
-  .cards-container {
-    display: flex;
-    flex-wrap: wrap;
-  }
-
-  .overlay {
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    background-color: rgba(0, 0, 0, 0.77);
-    transform: translate(-50%, -50%);
-    top: 50%;
-    left: 50%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    z-index: 10;
-  }
-
-  main {
-    height: 100vh;
-    width: 100vw;
-  }
-  .modal {
-    width: 750px;
-    background-color: white;
-    border-radius: 10px;
-    padding: 30px;
-    position: relative;
-    display: flex;
-    flex-direction: column;
-  }
-
-  .modal button {
-    padding: 10px 20px;
-    font-size: 20px;
-    width: 100%;
-    background-color: blueviolet;
-    border: none;
-    color: white;
-    cursor: pointer;
-    margin-top: 15px;
-
-  }
-
-  textarea {
-    width: 100%;
-    height: 200px;
-    padding: 5px;
-    font-size: 20px;
-  }
-.modal .close {
-  background-color: rgb(193, 15, 15);
-  margin-top: 7px;
-}
-.modal p{
-  color: red;
-}
-</style>
